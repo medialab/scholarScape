b90020caf88d043f682d8d441123cccb879da983
Romain Guillebert <romain.py@gmail.com>
2012-07-19 23:41:26 +0200

    Rewrite the isolation tests using flexmock

--- scholarScape/test/isolation/test_jsonrpc.py
+++ scholarScape/test/isolation/test_jsonrpc.py
@@ -1,16 +1,19 @@
 import json
 from scholarScape.server.rpc import scholarScape
+from scholarScape.server import rpc
 from mock import patch
+from flexmock import flexmock
 
-@patch("scholarScape.server.rpc.Duplicates")
 class TestJsonRPCIsolation:
-    def test_give_me_duplicates_no_cluster_id(self, Duplicates):
+    def test_give_me_duplicates_no_cluster_id(self):
         expected = [{"test1" : 1},{"test2" : 2},{"test3" : 3}]
 
-        Duplicates.get_cluster_with_possible_duplicates_left.return_value = 2
-        Duplicates.count_already_checked.return_value = 352
-        Duplicates.count_duplicates_left_for_cluster.return_value = 700
-        Duplicates.get_list_of_potential_duplicates.return_value = expected
+        Duplicates = flexmock(rpc.Duplicates)
+
+        Duplicates.should_receive("get_cluster_with_possible_duplicates_left").with_args(None, "project", "campaign").and_return(2).once()
+        Duplicates.should_receive("count_already_checked").with_args(None, "project", "campaign").and_return(352).once()
+        Duplicates.should_receive("count_duplicates_left_for_cluster").with_args(None, "project", "campaign", 2).and_return(700).once()
+        Duplicates.should_receive("get_list_of_potential_duplicates").with_args(None, "project", "campaign", 2).and_return(expected).once()
 
         jsonrpc = scholarScape(None)
         result = jsonrpc.jsonrpc_give_me_duplicates("project", "campaign", 3)
@@ -22,18 +25,15 @@ class TestJsonRPCIsolation:
             'cluster' : 2,
             }
 
-        Duplicates.get_cluster_with_possible_duplicates_left.assert_called_once_with(None, "project", "campaign")
-        Duplicates.count_already_checked.assert_called_once_with(None, "project", "campaign")
-        Duplicates.count_duplicates_left_for_cluster.assert_called_once_with(None, "project", "campaign", 2)
-        Duplicates.get_list_of_potential_duplicates.assert_called_once_with(None, "project", "campaign", 2)
-
-    def test_give_me_duplicates_with_cluster_id(self, Duplicates):
+    def test_give_me_duplicates_with_cluster_id(self):
         expected = [{"test1" : 1},{"test2" : 2},{"test3" : 3}]
 
-        Duplicates.get_cluster_with_possible_duplicates_left.return_value = 2
-        Duplicates.count_already_checked.return_value = 352
-        Duplicates.count_duplicates_left_for_cluster.return_value = 700
-        Duplicates.get_list_of_potential_duplicates.return_value = expected
+        Duplicates = flexmock(rpc.Duplicates)
+
+        Duplicates.should_receive("get_cluster_with_possible_duplicates_left").never()
+        Duplicates.should_receive("count_already_checked").with_args(None, "project", "campaign").and_return(352)
+        Duplicates.should_receive("count_duplicates_left_for_cluster").with_args(None, "project", "campaign", 2).and_return(700)
+        Duplicates.should_receive("get_list_of_potential_duplicates").with_args(None, "project", "campaign", 2).and_return(expected)
 
         jsonrpc = scholarScape(None)
         result = jsonrpc.jsonrpc_give_me_duplicates("project", "campaign", 3, 2)
@@ -45,14 +45,13 @@ class TestJsonRPCIsolation:
             'cluster' : 2,
             }
 
-        assert not Duplicates.get_cluster_with_possible_duplicates_left.called
-        Duplicates.count_already_checked.assert_called_once_with(None, "project", "campaign")
-        Duplicates.count_duplicates_left_for_cluster.assert_called_once_with(None, "project", "campaign", 2)
-        Duplicates.get_list_of_potential_duplicates.assert_called_once_with(None, "project", "campaign", 2)
+    def test_cannot_find_cluster(self):
+        Duplicates = flexmock(rpc.Duplicates)
 
-    def test_cannot_find_cluster(self, Duplicates):
-        Duplicates.get_cluster_with_possible_duplicates_left.return_value = 0
-        Duplicates.count_already_checked.return_value = 352
+        Duplicates.should_receive("count_duplicates_left_for_cluster").never()
+        Duplicates.should_receive("get_list_of_potential_duplicates").never()
+        Duplicates.should_receive("get_cluster_with_possible_duplicates_left").with_args(None, "project", "campaign").and_return(None)
+        Duplicates.should_receive("count_already_checked").with_args(None, "project", "campaign").and_return(352)
 
         jsonrpc = scholarScape(None)
         result = jsonrpc.jsonrpc_give_me_duplicates("project", "campaign", 3)
@@ -63,9 +62,3 @@ class TestJsonRPCIsolation:
             'duplicates' : [],
             'cluster' : -1,
             }
-
-        Duplicates.get_cluster_with_possible_duplicates_left.assert_called_once_with(None, "project", "campaign")
-        Duplicates.count_already_checked.assert_called_once_with(None, "project", "campaign")
-        assert not Duplicates.count_duplicates_left_for_cluster.called
-        assert not Duplicates.get_list_of_potential_duplicates.called
-
