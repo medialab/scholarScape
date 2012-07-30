25f52bd631fcfefc8f237f1ddeb8caa9e13a3789
Romain Guillebert <romain.py@gmail.com>
2012-07-30 17:23:15 +0200

    Set the scrapper's user agent to chrome 20

--- scholarScape/scholar/scholar/settings.py
+++ scholarScape/scholar/scholar/settings.py
@@ -11,7 +11,7 @@ BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['scholar.spiders']
 NEWSPIDER_MODULE = 'scholar.spiders'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6'
 
 DEPTH_LIMIT=3
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
@@ -28,24 +28,9 @@ SPIDER_MIDDLEWARES = {
 DOWNLOADER_MIDDLEWARES = {
     # 'scholar.middlewares.randomuseragent.RandomUserAgentMiddleware': None,
     # 'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': 400,
-	'scholar.middlewares.randomuseragent.RandomUserAgentMiddleware': 400,
 	'scholar.middlewares.proxymiddleware.ProxyMiddleware': 750,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
 }
-
-USER_AGENT_LIST = [
-"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6",
-"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
-"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
-"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
-"Mozilla/4.0 (compatible; MSIE 5.0; Windows NT 5.1; .NET CLR 1.1.4322)",
-"Opera/9.20 (Windows NT 6.0; U; en)",
-"Opera/9.00 (Windows NT 5.1; U; en)",
-"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.50",
-"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.0",
-"Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
-"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.5) Gecko/20060127 Netscape/8.1"
-]
 
 PROXY_URL="http://localhost:8118/"
 
